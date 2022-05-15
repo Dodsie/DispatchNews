79c8a3e256cd9d716763319e0216d2a95413bf1f
Dodsie <baileydods@gmail.com>
2022-05-14 20:24:03 -0600

    Add api fetch, update props

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+//"https://dummyimage.com/250x150/000/fff"
 const NewsCard = (props) => {
   return (
     <article id={props.index} className="articleContainer">
-      <img src="https://dummyimage.com/250x150/000/fff" alt={props.header} title={props.header} loading="lazy" />
+      <img src={props.image} alt={props.header} title={props.header} loading="lazy" />
       <h1>{props.header}</h1>
       <p>{props.description}</p>
       <p>Article: {props.index}</p>
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -7,7 +7,7 @@ const Weather = ()=>{
   const weatherJS = !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
 
   return (
-  <div style={{ width: '330px' }}>
+  <div>
     <a className="weatherwidget-io" href="https://forecast7.com/en/49d28n123d12/vancouver/" data-label_1="VANCOUVER" data-label_2="WEATHER" data-font="Roboto" data-icons="Climacons Animated" data-theme="weather_one" ></a>
     {weatherJS}
   </div>
