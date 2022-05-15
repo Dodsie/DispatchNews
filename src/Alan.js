d69a6be04797e0ebf316a7c1e8193fb4bb065b6c
MattSeligman <matt@modelitup.com>
2022-05-15 00:18:19 -0700

    Revised Layout SASS

--- src/Alan.js
+++ src/Alan.js
@@ -2,23 +2,9 @@
 // import alanBtn from "@alan-ai/alan-sdk-web";
 // import { useEffect } from "react";
 
-<<<<<<< HEAD
-export default function Alan() {
-    useEffect(() => {
-    alanBtn({
-      key:process.env.REACT_APP_ALAN_KEY,
-      onCommand: (commandData) => {
-        console.log(commandData)
-      }
-    });
-  }, []);
-  return 
-}
-=======
 // export default function Alan() {
 //     useEffect(() => {
 //     alanBtn({
-//       // key:'1c9f6524fef4a6b82540fef7e85415ab2e956eca572e1d8b807a3e2338fdd0dc/stage',
 //       key:process.env.REACT_APP_ALAN_KEY,
 //       onCommand: (commandData) => {
 //         if (command === 'newHeadlines') {
@@ -28,4 +14,3 @@ export default function Alan() {
 //   }, []);
 //   return 
 // }
->>>>>>> 79c8a3e256cd9d716763319e0216d2a95413bf1f
