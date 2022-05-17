6de9f20e16e02e7ba524a01d251083899abd1d8d
Alastair <alastair05@gmail.com>
2022-05-17 13:18:53 -0700

    Remove second server

---
+++ node-api-postgres/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const db = require("./queries");
+const app = express();
+const port = 3001;
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.get("/", (request, response) => {
+  response.json({
+    info: "Node.js, Express, and Postgres API",
+  });
+});
+
+app.get("/users", db.getUsers);
+
+app.get("/users/:id", db.getUserById);
+
+app.post("/users", db.createUser);
+
+app.get("/users/delete/:id", db.deleteUser);
+
+app.get("/favorite/:id", db.getFavorite);
+
+app.get("/addfav/:id", db.addFavorite);
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`);
+});
