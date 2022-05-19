403d27f982005a8745fb8d3c4049bd0c3188361b
Dodsie <baileydods@gmail.com>
2022-05-19 13:40:37 -0600

    fix delete favorites

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import App from './components/App';
 import { createRoot } from 'react-dom/client'
 import './polyfills'  
-  
+import { BrowserRouter } from "react-router-dom"
+
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<App />);
